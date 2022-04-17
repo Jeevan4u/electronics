a79e7b59456567ccf9839c9880177fa4fea8785b
Blank-lanaya <jeevankarki31@gmail.com>
2022-04-17 17:02:29 +0545

    home_page_complete

---
+++ owl-feautre.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:2000,
+    pullDrag:false,
+    nav:true,
+    dots:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
